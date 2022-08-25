d104de6031adc3e8948a279da51ad0dda08fd7f0
Ken Mueller <kenmueller0@gmail.com>
2022-08-25 14:15:06 -0700

    removed abc route

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.get('/abc', (_req, res) => {
-	res.send('abc')
-})
-
 app.use((req, _res, next) => {
 	console.log(req.method, req.url)
 	next()
